2cc1dd436e88c285066df5379010fde9b1cce8a7
Yarod <yescasyarod@gmail.com>
2022-12-21 23:41:23 -0600

    Add env variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className='text-xl font-bold'></h1>
+      <h1 className='text-xl font-bold'>Hola</h1>
     </>
   )
 }
